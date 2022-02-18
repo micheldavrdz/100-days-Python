314744ff845a155d1b8104016b761bf814dde146
Michel Rodriguez <micheldavrdz@gmail.com>
2022-02-17 22:26:10 -0600

    add: Day 1
    Band generator script

---
+++ Day 1 - Variables/band_generator.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program.
+name = input("Welcome to the Band Name Generator! MiWhat's your name?: \n")
+print(f"\nHello {name}!, let's make an easy band name\n")
+#2. Ask the user for their pet's name.
+pet = input("\nCould you tell me your pet's name?: \n")
+#3. Ask the user for the city that they grew up in.
+city = input("\nGreat! Now let's see ... what city do you live in?: \n")
+
+print(f"\nOh {name}, I got an idea!\nHow about we call your band {pet} {city}?\nPretty cool, huh?")
